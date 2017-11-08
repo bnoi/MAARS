d1a004922f7426752c20451f931f44926f66017d
Tong <tongli.bioinfo@gmail.com>
2017-11-08 22:46:37 +0100

    update-libs.sh updated
    Former-commit-id: 01ef9781a727246ebb11033afeb276485d3d776c

--- update-libs.sh
+++ update-libs.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
+mvn clean
 mvn install
-mvn dependency:copy-dependencies
 
 rm -f jars/*.jar
-rm -fr jars/MAARS_deps
 mkdir -p jars/MAARS_deps
-cp target/dependency/*.jar jars/MAARS_deps
 
-# Remove duplicated dependencies
-rm -f jars/MAARS_deps/3D_Viewer-*.jar
-rm -f jars/MAARS_deps/MMJ_*.jar
-rm -f jars/MAARS_deps/MMCoreJ*.jar
-rm -f jars/MAARS_deps/MMAcqEngine*.jar
-rm -f jars/MAARS_deps/miglayout*.jar
-rm -f jars/MAARS_deps/ij-*.jar
-rm -f jars/MAARS_deps/bsh-*.jar
-rm -f jars/MAARS_deps/clojure-*.jar
-rm -f jars/MAARS_deps/eventbus-*.jar
-rm -f jars/MAARS_deps/gentyref-*.jar
-rm -f jars/MAARS_deps/guava-*.jar
-rm -f jars/MAARS_deps/jcommon-*.jar
-rm -f jars/MAARS_deps/jfreechart-*.jar
-rm -f jars/MAARS_deps/rsyntaxtextarea-*.jar
-rm -f jars/MAARS_deps/kryo-*.jar
-rm -f jars/MAARS_deps/minlog-*.jar
-rm -f jars/MAARS_deps/objenesis-*.jar
-
-cp target/MAARS_1.0.0.jar jars/
-git checkout jars/MAARS_deps/Adjustable_Watershed.class
+maars_folders=(maars_bfSeg maars_lib maars_otf maars_post)
+for f in ${maars_folders[*]}
+do
+  cp $f/target/$f*-SNAPSHOT.jar jars/
+done
+echo libs updated!
