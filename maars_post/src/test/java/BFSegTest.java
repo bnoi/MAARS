672ede26c739d3b41b9090647dc2760383f688f8
Tong LI <tongli.bioinfo@gmail.com>
2018-02-14 16:26:00 +0100

    first test with imglib2
    Former-commit-id: 21046d58135f3df5087f6954dbe23ab0b8dbb826

--- maars_post/src/test/java/BFSegTest.java
+++ maars_post/src/test/java/BFSegTest.java
@@ -1,17 +1,9 @@
-import ij.ImageJ;
-import io.scif.config.SCIFIOConfig;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
 import io.scif.img.SCIFIOImgPlus;
-import maars.headless.batchSegmentation.DefaultBatchSegmentation;
-import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
 import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.cell.CellImg;
-import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.NumericType;
-import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +11,6 @@ import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -29,28 +19,37 @@ import java.util.List;
 public class  BFSegTest {
    private static Logger logger = LoggerFactory.getLogger(BFSegTest.class);
    @Parameterized.Parameters
-   public static < T extends NumericType< T > & NativeType< T > > Collection<Object[]> prepareFiles() throws ImgIOException {
+   public static Collection<Object[]> prepareFiles() throws ImgIOException {
       String root = System.getProperty("user.dir") + "/src/main/resources/";
       ImgOpener imgOpener = new ImgOpener();
-      SCIFIOConfig config = new SCIFIOConfig();
-      config.imgOpenerSetImgModes( SCIFIOConfig.ImgMode.PLANAR );
-      List<SCIFIOImgPlus<?>> bfimages =  imgOpener.openImgs( root + "BF.tif", config);
-      List<SCIFIOImgPlus<?>> fluoimages =  imgOpener.openImgs( root + "FLUO.tif", config);
-
+      ImgFactory<FloatType> factory = new ArrayImgFactory<>();
+      List<SCIFIOImgPlus<FloatType>> bfimages =  imgOpener.openImgs( root + "BF.tif", factory, new FloatType());
+      List<SCIFIOImgPlus<FloatType>> fluoimages =  imgOpener.openImgs( root + "FLUO.tif", factory, new FloatType());
       return Arrays.asList(new Object[][] {{bfimages.get(0), fluoimages.get(0)}});
    }
    @Parameterized.Parameter
-   public SCIFIOImgPlus bfimg;
+   public SCIFIOImgPlus <FloatType> bfimg;
 
    @Parameterized.Parameter(1)
-   public SCIFIOImgPlus fluoimg;
+   public SCIFIOImgPlus <FloatType> fluoimg;
 
    @Test
    public void show(){
       ImageJFunctions.show( bfimg );
       ImageJFunctions.show( fluoimg );
+//      OpService ops = new DefaultOpService();
+//      FinalInterval finalInterval = new FinalInterval(0,0,5,5);
+//
+//      RandomAccessibleInterval<UnsignedByteType> view = Views.interval(bfimg.getImg(), finalInterval);
+//      ImageJFunctions.show( view );
+//      logger.info((int) ops.run(Ops.Math.Add.class, 1,2) + "");
+//      int x = (int) ops.run("math.add", 1,1);
+//      System.out.println(x);
+//      RandomAccessibleInterval
+//      logger.info(x + "");
+//      ImageJFunctions.show( img. );
       try {
-         Thread.sleep(10000);
+         Thread.sleep(100000);
       } catch (InterruptedException e) {
          e.printStackTrace();
       }
